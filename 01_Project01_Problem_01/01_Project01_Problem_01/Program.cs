7f396bb6ffb3f88693ee39d7e32c0ea8cf785190
Hüseyin Bilginer <bilginerhuseyinofficial@gmail.com>
2019-06-26 00:55:15 +0300

    01_Project01_Problem_01 Finished

---
+++ 01_Project01_Problem_01/01_Project01_Problem_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01_Project01_Problem_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, IT, FI;
+
+            Console.Write("Düzgün Çokgenin Kenar Sayısını Giriniz: ");
+            n = int.Parse(Console.ReadLine());
+
+            FI = 180 - (360 / n);
+
+            IT = FI * n;
+
+            Console.WriteLine("Düzgün Çokgenin Tüm İç Açılar Toplamı:" + IT);
+
+            Console.WriteLine("Düzgün Çokgenin Bir İç Açısı:{0} --Düzgün Çokgenin Tüm İç Açılar Toplamı:{1}",FI,IT);
+            
+        }
+    }
+}
